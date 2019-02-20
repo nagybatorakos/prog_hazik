6ffd8d6509f070a471a5023960cad5fca7dc4609
szora <szorakozza@gmail.com>
2019-02-20 13:42:03 +0100

    hf

--- gyakhazi_1.py
+++ gyakhazi_1.py
@@ -1,13 +1,23 @@
 #5
 
-def sorrend(n):
-    ls=n.split(",")
+# def sorrend(n):
+#     ls=n.split(",")
+#
+#
+#
+#     return sorted(ls)
+#
+#
+# n=input()
+# ls=[n]
+# print(sorrend(n))
 
+def lanc(szoveg, n):
+    if len(szoveg)>2 and n>2:
+        return szoveg[:n]
+    else:
+        return szoveg
 
-
-    return sorted(ls)
-
-
-n=input()
-ls=[n]
-print(sorrend(n))
+str=input()
+n=int(input())
+print(lanc(str,n))
